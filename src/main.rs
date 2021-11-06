500effc65fb8caa48644a4319138a7c270d345a0
Florin Balate <florin.balate@gmail.com>
2021-11-06 17:30:13 +0200

    added Search Form

--- src/main.rs
+++ src/main.rs
@@ -2,30 +2,47 @@
 
 use rocket_dyn_templates::Template;
 use rocket::serde::Serialize;
+use rocket::form::Form;
+use rocket::request::FlashMessage;
+use rocket::response::{Flash, Redirect};
 use rocket::fs::{FileServer, relative};
 
-#[derive(Debug,Serialize,Clone)]
+#[derive(Debug, Serialize)]
 #[serde(crate = "rocket::serde")]
-pub struct Data<'a> {
-    pub title: String,
-    pub name: Option<String>,
-    pub items: Vec<&'a str>
+struct Context {
+    flash: Option<(String, String)>,
+    txs: Vec<String>
 }
 
-impl Data<'_> {
-    fn new() -> Data<'static> {
-        Data{
-        title: String::from("Hello"),
-        name: Option::from(String::from("name")),
-        items: vec!["One", "Two", "Three"],
-        }
+impl Context{
+    pub fn new(flash: Option<(String,String)>) -> Context {
+        Context {flash, txs: vec![]}
     }
 }
 
+#[derive(Debug, FromForm)]
+pub struct Search{
+    pub address: String,
+}
+
 #[get("/")]
-fn index() -> Template {
-    let context = Data::new();
-    Template::render("index", &context)
+fn index(flash: Option<FlashMessage<'_>>) -> Template {
+    let flash = flash.map(FlashMessage::into_inner);
+    Template::render("index", Context::new(flash))
+}
+
+#[post("/address", data = "<search_form>")]
+fn search(search_form: Form<Search>) -> Flash<Redirect> {
+    let address = search_form.into_inner().address;
+    println!("<{0}>", address);
+    if address.is_empty() {
+        Flash::error(Redirect::to("/"), "Address cannot be empty.")
+    // } else if let Err(e) = Task::insert(todo, &conn).await {
+    //     error_!("DB insertion error: {}", e);
+    //     Flash::error(Redirect::to("/"), "Todo could not be inserted due an internal error.")
+    } else {
+        Flash::success(Redirect::to("/"), "Address searched successfully.")
+    }
 }
 
 #[launch]
@@ -33,5 +50,5 @@ fn rocket() -> _ {
     rocket::build()
     .attach(Template::fairing())
     .mount("/", FileServer::from(relative!("static")))
-    .mount("/", routes![index])
+    .mount("/", routes![index, search])
 }
