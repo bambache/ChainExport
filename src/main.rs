0d663526f9e4a8811baa9baa88c91e550f0e6ba1
Florin Balate <florin.balate@gmail.com>
2021-11-19 19:27:40 +0200

    * rpc handler takes address * list txs function supports pagination * list txs function searches both sender and recipient txs

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,61 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
-use rocket_dyn_templates::Template;
-use rocket::serde::{Serialize, Deserialize};
+use std::fmt::Write;
+
 use rocket::form::Form;
+use rocket::fs::{relative, FileServer};
+use rocket::http::{ContentType, Status};
 use rocket::request::FlashMessage;
 use rocket::response::{Flash, Redirect};
-use rocket::fs::{FileServer, relative};
-use rocket::{State, fairing::AdHoc};
-use std::fmt::Write;
-use tendermint_rpc::{HttpClient,Client, Order};
-use tendermint_rpc::query::Query;
+use rocket::serde::{Deserialize, Serialize};
+use rocket::{fairing::AdHoc, State};
+use rocket_dyn_templates::Template;
 
+use tendermint_rpc::error::Error;
+use tendermint_rpc::query::Query;
+use tendermint_rpc::{Client, HttpClient, Order};
 
 #[derive(Debug, Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Tx {
     hash: String,
-    height: u64
+    height: u64,
 }
 
 #[derive(Debug, Serialize)]
 #[serde(crate = "rocket::serde")]
 struct Context {
     flash: Option<(String, String)>,
-    txs: Vec<Tx>
+    txs: Vec<Tx>,
 }
 
-impl Context{
-    pub fn err(flash: Option<(String,String)>) -> Context {
-        Context {flash, txs: vec![]}
+impl Context {
+    pub fn err(flash: Option<(String, String)>) -> Context {
+        Context { flash, txs: vec![] }
     }
     pub fn new(txs: Vec<Tx>) -> Context {
-        Context {flash: None, txs}
+        Context { flash: None, txs }
     }
 }
 
 #[derive(Debug, FromForm)]
-struct Search{
+struct Search {
     address: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(crate = "rocket::serde")]
-struct Chain{
+struct Chain {
     id: String,
     api: String,
-    prefix: String
+    prefix: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
 struct Chains {
-    chains: Vec<Chain>
+    chains: Vec<Chain>,
 }
 
 #[get("/")]
@@ -70,7 +74,10 @@ fn get_chain_matching_prefix<'a>(address: &'a String, chains: &'a Vec<Chain>) ->
 }
 
 #[post("/address", data = "<search_form>")]
-async fn search(search_form: Form<Search>, config: &State<Chains>) -> Result<Template,Flash<Redirect>> {
+async fn search(
+    search_form: Form<Search>,
+    config: &State<Chains>,
+) -> Result<Template, Flash<Redirect>> {
     let address = search_form.into_inner().address;
     println!("<{0}>", address);
     if address.is_empty() {
@@ -78,91 +85,104 @@ async fn search(search_form: Form<Search>, config: &State<Chains>) -> Result<Tem
     }
 
     match get_chain_matching_prefix(&address, &config.chains) {
-        None => Err(Flash::error(Redirect::to("/"), "Address prefix is not supported.")),
-        Some(chain) => {
-        let v = list_txs_for_address(&address, chain).await;
-        Ok(Template::render("index", Context::new(v)))
-        }
+        None => Err(Flash::error(
+            Redirect::to("/"),
+            "Address prefix is not supported.",
+        )),
+        Some(chain) => match list_txs_for_address(&address, chain).await {
+            Ok(v) => Ok(Template::render("index", Context::new(v))),
+            Err(e) => Err(Flash::error(Redirect::to("/"), e.to_string())),
+        },
     }
 }
 
-async fn list_txs_for_address(address: &String, chain: &Chain) -> Vec<Tx> {
-    let client = HttpClient::new(&chain.api[0..])
-        .unwrap();
+async fn list_txs_for_address(address: &String, chain: &Chain) -> Result<Vec<Tx>, Error> {
+    // // TODO add unit tests for the following addresses
+    // let query = Query::eq("transfer.sender", "ubik18dv926f68dtq32v54zrc5982q2wktgp5jevvft");
+    // // let query = Query::eq("transfer.sender", "cosmos18dv926f68dtq32v54zrc5982q2wktgp5m07qf9");
+    // // no OR atm .or_eq("transfer.sender", "ubik18dv926f68dtq32v54zrc5982q2wktgp5jevvft");
+    // // let query = Query::eq("tx.height", 1090159);
 
+    let client = HttpClient::new(&chain.api[0..])?;
     let mut result = Vec::new();
-    let query = Query::eq("transfer.sender", &address[0..]);
+    let queries = vec![
+        Query::eq("transfer.sender", &address[0..]),
+        Query::eq("transfer.recipient", &address[0..]),
+    ];
 
-    let txs = client.tx_search(query,true,1,40,Order::Descending)
-        .await
-        .unwrap();
+    let per_page = 10u8;
 
-    for tx in txs.txs.iter() {
-        result.push(Tx{hash: tx.hash.to_string(), height: tx.height.value()});
-        // let events = &tx.tx_result.events;
-        // for ev in events.iter() {
-        //     if ev.type_str == "transfer" {
-        //         writeln!(result, "\tEv:\t{:?}", ev.type_str).unwrap();
-        //         for attr in ev.attributes.iter() {
-        //             writeln!(result, "\t\t{:?}->{:?}", attr.key, attr.value).unwrap();
-        //         }
-        //     }
-        // }
-    }
-    
-    result
-}
+    for query in queries.iter() {
+        let mut count = 0u32;
+        let mut page = 0u32;
+        loop {
+            page += 1;
+            print!(
+                "call tx_search with query {:}, page{:?}",
+                query.to_string(),
+                page
+            );
+            let txs = client
+                .tx_search(query.clone(), true, page, per_page, Order::Descending)
+                .await?;
 
-#[get("/rpc")]
-async fn rpc() -> String {
-    let client = HttpClient::new("http://178.18.242.126:26657")
-        .unwrap();
-
-    let mut result = String::new();
-
-    let query = Query::eq("transfer.sender", "ubik18dv926f68dtq32v54zrc5982q2wktgp5jevvft");
-    // no OR atm .or_eq("transfer.sender", "ubik18dv926f68dtq32v54zrc5982q2wktgp5jevvft");
-    // let query = Query::eq("tx.height", 1090159);
-    writeln!(result,"Show Query {:}", query.to_string()).unwrap();
-
-    let txs = client.tx_search(query,true,1,40,Order::Descending)
-        .await
-        .unwrap();
-
-    writeln!(result,"Query: {:?}", txs.total_count).unwrap();
-
-    for tx in txs.txs.iter() {
-        writeln!(result,"Hash:\t{:?}", tx.hash).unwrap();
-        writeln!(result,"Height:\t{:?}", tx.height).unwrap();
-        writeln!(result,"Gas (used / wanted):\t{:?} / {:?}"
-            , tx.tx_result.gas_used
-            , tx.tx_result.gas_wanted)
-                .unwrap();
-        let events = &tx.tx_result.events;
-        for ev in events.iter() {
-            if ev.type_str == "transfer" {
-                writeln!(result, "\tEv:\t{:?}", ev.type_str).unwrap();
-                for attr in ev.attributes.iter() {
-                    writeln!(result, "\t\t{:?}->{:?}", attr.key, attr.value).unwrap();
+            for tx in txs.txs.iter() {
+                count += 1;
+                result.push(Tx {
+                    hash: tx.hash.to_string(),
+                    height: tx.height.value(),
+                });
+                // writeln!(result,"Gas (used / wanted):\t{:?} / {:?}"
+                //     , tx.tx_result.gas_used
+                //     , tx.tx_result.gas_wanted)
+                //         .unwrap();
+                let events = &tx.tx_result.events;
+                for ev in events.iter() {
+                    if ev.type_str == "transfer" {
+                        print!("\tEv:\t{:?}", ev.type_str);
+                        for attr in ev.attributes.iter() {
+                            println!("\t\t{:?}->{:?}", attr.key, attr.value);
+                        }
+                    }
                 }
+            }
+
+            if txs.total_count == count {
+                break;
             }
         }
     }
 
-    result
+    Ok(result)
+}
+
+#[get("/rpc/<address>")]
+async fn rpc(address: String, config: &State<Chains>) -> (Status, (ContentType, String)) {
+    match get_chain_matching_prefix(&address, &config.chains) {
+        None => (
+            Status::NotFound,
+            (
+                ContentType::Plain,
+                "Unsupported address prefix!".to_string(),
+            ),
+        ),
+        Some(chain) => match list_txs_for_address(&address, chain).await {
+            Ok(v) => (Status::Ok, (ContentType::Plain, format!("{:?}", v))),
+            Err(e) => (Status::NotFound, (ContentType::Plain, e.to_string())),
+        },
+    }
 }
 
 #[get("/chains")]
 fn chains(config: &State<Chains>) -> String {
-    // config.chains.get(0).cloned().unwrap_or("default".into())
     let mut result = String::new();
     let mut idx = 0u8;
     for chain in config.chains.iter() {
-        idx +=1;
-        writeln!(result,"chain-{}:", idx).unwrap();
-        writeln!(result,"id:\t{}",chain.id).unwrap();
-        writeln!(result,"api:\t{}",chain.api).unwrap();
-        writeln!(result,"prefix:\t{}",chain.prefix).unwrap();
+        idx += 1;
+        writeln!(result, "chain-{}:", idx).unwrap();
+        writeln!(result, "id:\t{}", chain.id).unwrap();
+        writeln!(result, "api:\t{}", chain.api).unwrap();
+        writeln!(result, "prefix:\t{}", chain.prefix).unwrap();
     }
     result
 }
@@ -170,8 +190,8 @@ fn chains(config: &State<Chains>) -> String {
 #[launch]
 fn rocket() -> _ {
     rocket::build()
-    .attach(Template::fairing())
-    .mount("/", FileServer::from(relative!("static")))
-    .mount("/", routes![index, search, chains, rpc])
-    .attach(AdHoc::config::<Chains>())
+        .attach(Template::fairing())
+        .mount("/", FileServer::from(relative!("static")))
+        .mount("/", routes![index, search, chains, rpc])
+        .attach(AdHoc::config::<Chains>())
 }
